3d5b27b103f952df6e15ae3b3b49b02e3f6239f1
quanyuchen2000 <quanyuchen@mail.ustc.edu.cn>
2025-04-18 17:49:24 +0800

    better restrict residual

--- homogenization/grid.cu
+++ homogenization/grid.cu
@@ -1003,53 +1003,97 @@ __global__ void restrict_residual_kernel_host_H(
 	if (pos.x == 0 || pos.x == MIN_TRANSFER) { ratio *= 0.5; }
 	if (pos.y == 0 || pos.y == MIN_TRANSFER) { ratio *= 0.5; }
 	if (pos.z == 0 || pos.z == MIN_TRANSFER) { ratio *= 0.5; }
-
+	int posvec[3] = { pos.x, pos.y, pos.z };
 	if (!fiction && !vflag.is_period_padding()) {
-		for (int offx = -coarseRatio[0] + 1; offx < coarseRatio[0]; offx++) {
-			for (int offy = -coarseRatio[1] + 1; offy < coarseRatio[1]; offy++) {
-				for (int offz = -coarseRatio[2] + 1; offz < coarseRatio[2]; offz++) {
-					// for reso == 32 pos is from 1 to 33
-					int off_pos[3] = { pos.x + offx, pos.y + offy, pos.z + offz };
-					// check if off_pos reffer to a coarse vertex
-					// if no referred skip
-					if (off_pos[0] % coarseRatio[0] == 0 && off_pos[1] % coarseRatio[1] == 0 && off_pos[2] % coarseRatio[2] == 0) {
-						// weight is still the same
-						float w = (coarseRatio[0] - abs(offx)) * (coarseRatio[1] - abs(offy)) * (coarseRatio[2] - abs(offz)) / pr;
-						// add to that gF_H
-						// coarse pos block:
-						int c_pos[3] = { off_pos[0] / coarseRatio[0], off_pos[1] / coarseRatio[1], off_pos[2] / coarseRatio[2] };
-						// true coarse pos by adding block
-						int c_true_pos[3];
-						for (int pos_idx = 0; pos_idx < 3; pos_idx++) {
-							c_true_pos[pos_idx] = (MIN_TRANSFER / coarseRatio[pos_idx]) * bid[pos_idx] + c_pos[pos_idx];
-						}
-						// printf("fine pos: (%d, %d, %d) coarse pos:(%d, %d, %d)\n",pos.x, pos.y, pos.z, c_true_pos[0], c_true_pos[1], c_true_pos[2]);
-
-						// if c_true_pos on the edges do to the opposite pos too
-						int xp[2] = {c_true_pos[0], -1};
-						int yp[2] = {c_true_pos[1], -1};
-						int zp[2] = {c_true_pos[2], -1};
-						if (xp[0] == 0) { xp[1] = gGridCellReso[0]; }
-						if (xp[0] == gGridCellReso[0]) { xp[1] = 0; }
-						if (yp[0] == 0) { yp[1] = gGridCellReso[1]; }
-						if (yp[0] == gGridCellReso[1]) { yp[1] = 0; }
-						if (zp[0] == 0) { zp[1] = gGridCellReso[2]; }
-						if (zp[0] == gGridCellReso[2]) { zp[1] = 0; }
-						for (int i = 0; i < 8; i++) {
-							int idx = i % 2;
-							int idy = i / 2 % 2;
-							int idz = i / 4;
-							int reflect[3];
-							if (xp[idx] != -1 && yp[idy] != -1 && zp[idz] != -1) {
-								reflect[0] = xp[idx]; reflect[1] = yp[idy]; reflect[2] = zp[idz];
-								int coarse_lexi = lexi2gs(reflect, gGsVertexReso, gGsVertexEnd);
-								atomicAdd(&gF_H[0][coarse_lexi], float(gRfine_H[0][tid]) * w * ratio);
-							}
-						}
+		int upper_vertex[3][2] = { {-1,-1,},{-1,-1},{-1,-1} };
+#pragma unroll
+		for (int i = 0; i < 3; i++) {
+			upper_vertex[i][0] = posvec[i] / coarseRatio[i] * coarseRatio[i];
+			if (posvec[i] % coarseRatio[i] != 0) {
+				upper_vertex[i][1] = (posvec[i] / coarseRatio[i] + 1) * coarseRatio[i];
+			}
+		}
+		for (int i = 0; i < 8; i++) {
+			int iddx = i % 2;
+			int iddy = i / 2 % 2;
+			int iddz = i / 4;
+			if (upper_vertex[0][iddx] != -1 && upper_vertex[1][iddy] != -1 && upper_vertex[2][iddz] != -1) {
+				int off_pos[3] = { upper_vertex[0][iddx] , upper_vertex[1][iddy] , upper_vertex[2][iddz] };
+				float w = (coarseRatio[0] - abs(off_pos[0] - posvec[0])) * (coarseRatio[1] - abs(off_pos[1] - posvec[1])) * (coarseRatio[2] - abs(off_pos[2] - posvec[2])) / pr;
+				int c_pos[3] = { off_pos[0] / coarseRatio[0], off_pos[1] / coarseRatio[1], off_pos[2] / coarseRatio[2] };
+				int c_true_pos[3];
+#pragma unroll
+				for (int pos_idx = 0; pos_idx < 3; pos_idx++) {
+					c_true_pos[pos_idx] = (MIN_TRANSFER / coarseRatio[pos_idx]) * bid[pos_idx] + c_pos[pos_idx];
+				}
+				int xp[2] = { c_true_pos[0], -1 };
+				int yp[2] = { c_true_pos[1], -1 };
+				int zp[2] = { c_true_pos[2], -1 };
+				if (xp[0] == 0) { xp[1] = gGridCellReso[0]; }
+				if (xp[0] == gGridCellReso[0]) { xp[1] = 0; }
+				if (yp[0] == 0) { yp[1] = gGridCellReso[1]; }
+				if (yp[0] == gGridCellReso[1]) { yp[1] = 0; }
+				if (zp[0] == 0) { zp[1] = gGridCellReso[2]; }
+				if (zp[0] == gGridCellReso[2]) { zp[1] = 0; }
+#pragma unroll
+				for (int i = 0; i < 8; i++) {
+					int idx = i % 2;
+					int idy = i / 2 % 2;
+					int idz = i / 4;
+					int reflect[3];
+					if (xp[idx] != -1 && yp[idy] != -1 && zp[idz] != -1) {
+						reflect[0] = xp[idx]; reflect[1] = yp[idy]; reflect[2] = zp[idz];
+						int coarse_lexi = lexi2gs(reflect, gGsVertexReso, gGsVertexEnd);
+						atomicAdd(&gF_H[0][coarse_lexi], float(gRfine_H[0][tid]) * w * ratio);
 					}
 				}
 			}
 		}
+
+		//for (int offx = -coarseRatio[0] + 1; offx < coarseRatio[0]; offx++) {
+		//	for (int offy = -coarseRatio[1] + 1; offy < coarseRatio[1]; offy++) {
+		//		for (int offz = -coarseRatio[2] + 1; offz < coarseRatio[2]; offz++) {
+		//			// for reso == 32 pos is from 1 to 33
+		//			int off_pos[3] = { pos.x + offx, pos.y + offy, pos.z + offz };
+		//			// check if off_pos reffer to a coarse vertex
+		//			// if no referred skip
+		//			if (off_pos[0] % coarseRatio[0] == 0 && off_pos[1] % coarseRatio[1] == 0 && off_pos[2] % coarseRatio[2] == 0) {
+		//				// weight is still the same
+		//				float w = (coarseRatio[0] - abs(offx)) * (coarseRatio[1] - abs(offy)) * (coarseRatio[2] - abs(offz)) / pr;
+		//				// add to that gF_H
+		//				// coarse pos block:
+		//				int c_pos[3] = { off_pos[0] / coarseRatio[0], off_pos[1] / coarseRatio[1], off_pos[2] / coarseRatio[2] };
+		//				// true coarse pos by adding block
+		//				int c_true_pos[3];
+		//				for (int pos_idx = 0; pos_idx < 3; pos_idx++) {
+		//					c_true_pos[pos_idx] = (MIN_TRANSFER / coarseRatio[pos_idx]) * bid[pos_idx] + c_pos[pos_idx];
+		//				}
+		//				// printf("fine pos: (%d, %d, %d) coarse pos:(%d, %d, %d)\n",pos.x, pos.y, pos.z, c_true_pos[0], c_true_pos[1], c_true_pos[2]);
+		//				// if c_true_pos on the edges do to the opposite pos too
+		//				int xp[2] = {c_true_pos[0], -1};
+		//				int yp[2] = {c_true_pos[1], -1};
+		//				int zp[2] = {c_true_pos[2], -1};
+		//				if (xp[0] == 0) { xp[1] = gGridCellReso[0]; }
+		//				if (xp[0] == gGridCellReso[0]) { xp[1] = 0; }
+		//				if (yp[0] == 0) { yp[1] = gGridCellReso[1]; }
+		//				if (yp[0] == gGridCellReso[1]) { yp[1] = 0; }
+		//				if (zp[0] == 0) { zp[1] = gGridCellReso[2]; }
+		//				if (zp[0] == gGridCellReso[2]) { zp[1] = 0; }
+		//				for (int i = 0; i < 8; i++) {
+		//					int idx = i % 2;
+		//					int idy = i / 2 % 2;
+		//					int idz = i / 4;
+		//					int reflect[3];
+		//					if (xp[idx] != -1 && yp[idy] != -1 && zp[idz] != -1) {
+		//						reflect[0] = xp[idx]; reflect[1] = yp[idy]; reflect[2] = zp[idz];
+		//						int coarse_lexi = lexi2gs(reflect, gGsVertexReso, gGsVertexEnd);
+		//						atomicAdd(&gF_H[0][coarse_lexi], float(gRfine_H[0][tid]) * w * ratio);
+		//					}
+		//				}
+		//			}
+		//		}
+		//	}
+		//}
 	}
 }
 
